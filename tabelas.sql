d7fee07ec9bc4842a6e8d82ec5fe4a69626bd838
Marisang <mari.sangean@gmail.com>
2026-03-27 18:13:07 -0300

    Update tabelas.sql

--- tabelas.sql
+++ tabelas.sql
@@ -18,22 +18,22 @@ CREATE TABLE if NOT exists pedidos (
   FOREIGN KEY (produto_id) REFERENCES produtos (id)
   );
 INSERT INTO clientes (id, nome, cidade) VALUes
-  (1, 'Ana', 'São Paulo')
-  (2, 'Bruno', 'Rio de Janeiro')
-  (3, 'Carla', 'São Paulo')
-  (4, 'Diego', 'Belo Horizonte')
-  (5, 'Thomas', 'São Paulo')
+  (1, 'Ana', 'São Paulo'),
+  (2, 'Bruno', 'Rio de Janeiro'),
+  (3, 'Carla', 'São Paulo'),
+  (4, 'Diego', 'Belo Horizonte'),
+  (5, 'Thomas', 'São Paulo');
 INSERT INTO produtos (id, nome, preco, categoria) VALUES
-  (1, 'Elden Ring', 249.90, 'RPG')
-  (2, 'Minecraft', 99.90, 'Sandbox')
-  (3, 'Hollow Knight', 46.99, 'Aventura')
-  (4, 'GTA VI', 79.90, 'Aventura')
-  (5, 'Ciberpunk 2077', 199.90, 'RPG')
-INSERT INTO pedidos (id, cliente_id, produto_id, quantidade)
-  (1, 1, 1, 2)
-  (2, 1, 3, 1)
-  (3, 2, 5, 1)
-  (4, 3, 1, 3)
-  (5, 3, 2, 2)
-  (6, 5, 4, 1)
-  (7, 5, 3, 4)
+  (1, 'Elden Ring', 249.90, 'RPG'),
+  (2, 'Minecraft', 99.90, 'Sandbox'),
+  (3, 'Hollow Knight', 46.99, 'Aventura'),
+  (4, 'GTA VI', 79.90, 'Aventura'),
+  (5, 'Ciberpunk 2077', 199.90, 'RPG');
+INSERT INTO pedidos (id, cliente_id, produto_id, quantidade) VALUES
+  (1, 1, 1, 2),
+  (2, 1, 3, 1),
+  (3, 2, 5, 1),
+  (4, 3, 1, 3),
+  (5, 3, 2, 2),
+  (6, 5, 4, 1),
+  (7, 5, 3, 4);
